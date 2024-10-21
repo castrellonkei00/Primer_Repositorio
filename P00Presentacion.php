4354496524f1141e3c389bc4554225493e1f6515
keira_castrellon_rodriguez <keira.kastrellonr@gmail.com>
2024-10-21 09:31:04 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,12 @@
+<? 
+/* CBTIS 89
+P00Presentacion
+Programa contiene mis datos personales 
+keira yajaira castrellon rodriguez
+3°"A" Programacion TM*/
+
+echo "Mi nombre es Keira Yajaira Castrellon Rodriguez";
+echo"<br>","Estudio en el CBTIS #89";
+echo "<br>","En 3er semestre de la carrera de Programacion A del Turno Matutino";
+echo "<br>","vivo en la ciudad de Durango,Dgo";
+?>
